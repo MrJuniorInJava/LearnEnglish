7e0ad1fa9ccb64bba5aa69f011d2e38f5fc1d257
Nicks1337 <tolyan.kolpakov1234@gmail.com>
2023-11-09 20:42:09 +0500

    Добавить cортировку в алфавитном порядке при выводе всех карточек

--- src/main/java/ru/kolpakov/LearnEnglish/services/WordService.java
+++ src/main/java/ru/kolpakov/LearnEnglish/services/WordService.java
@@ -2,12 +2,15 @@ package ru.kolpakov.LearnEnglish.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kolpakov.LearnEnglish.models.Word;
 import ru.kolpakov.LearnEnglish.repositories.WordsRepository;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class WordService {
     private final WordsRepository wordsRepository;
 
@@ -15,14 +18,18 @@ public class WordService {
     public WordService(WordsRepository wordsRepository) {
         this.wordsRepository = wordsRepository;
     }
+    @Transactional
     public void addWord(Word word){
         wordsRepository.save(word);
     }
+    @Transactional
     public void deleteWordById(int id){
         wordsRepository.deleteById(id);
     }
     public List<Word> findAll(){
-        return  wordsRepository.findAll();
+        List<Word> words = wordsRepository.findAll();
+        words.sort(Comparator.comparing(Word::getName));
+        return  words;
     }
     public Word findWordById(int id){
         return wordsRepository.findById(id).orElse(null);
